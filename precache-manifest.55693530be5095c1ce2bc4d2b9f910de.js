f6a2481c3bc1d25570469b996d2eef2b60152afb
Jason Liu <jliu@Jasons-MacBook-Pro-2.local>
2019-05-22 19:27:37 -0400

    Updates

---
+++ precache-manifest.55693530be5095c1ce2bc4d2b9f910de.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d9f07e5ad25f14e14a1b06c952e85646",
+    "url": "/campaign-creator-demo/index.html"
+  },
+  {
+    "revision": "75cb32bcfcfdfd0064ed",
+    "url": "/campaign-creator-demo/static/css/main.ba768d7e.chunk.css"
+  },
+  {
+    "revision": "a1165f6cab6d810a5062",
+    "url": "/campaign-creator-demo/static/js/2.6c96682a.chunk.js"
+  },
+  {
+    "revision": "75cb32bcfcfdfd0064ed",
+    "url": "/campaign-creator-demo/static/js/main.79432226.chunk.js"
+  },
+  {
+    "revision": "e0f485a671e68288c1f2",
+    "url": "/campaign-creator-demo/static/js/runtime~main.217be79d.js"
+  },
+  {
+    "revision": "9542484b814970f17ef820ef78b102f7",
+    "url": "/campaign-creator-demo/static/media/Hyperlocology-Logo.9542484b.png"
+  },
+  {
+    "revision": "cb679af3f2a0d35dc51d55e2bf384a59",
+    "url": "/campaign-creator-demo/static/media/Info-Icon.cb679af3.png"
+  },
+  {
+    "revision": "4d5f1004e11d5d42f0e472425d068ead",
+    "url": "/campaign-creator-demo/static/media/Location-Icon.4d5f1004.png"
+  },
+  {
+    "revision": "cbbbb61a72221bdada9a5f4f230c1616",
+    "url": "/campaign-creator-demo/static/media/Setting-Icon.cbbbb61a.png"
+  },
+  {
+    "revision": "2a869073cb86832916529d652a7d797c",
+    "url": "/campaign-creator-demo/static/media/Targeting-Icon.2a869073.png"
+  },
+  {
+    "revision": "37d75d8f1dac7e1dd318b8a63877dd05",
+    "url": "/campaign-creator-demo/static/media/avatar.37d75d8f.jpg"
+  },
+  {
+    "revision": "65fd041b032719c5314f3fb7b531e003",
+    "url": "/campaign-creator-demo/static/media/check.65fd041b.svg"
+  },
+  {
+    "revision": "219610f547b7b68019a2ee73494c8789",
+    "url": "/campaign-creator-demo/static/media/map.219610f5.svg"
+  }
+]);
