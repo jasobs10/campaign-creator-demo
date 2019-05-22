bdee4d2471c74fbb65c6b98ea0ba8ca5a94379f8
Jason Liu <jliu@Jasons-MBP-2.fios-router.home>
2019-05-22 17:13:48 -0400

    add segment select

---
+++ src/app/create-campaign/segment-select-container/segment-select-container.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import './segment-select-container.scss';
+
+class SegmentSelectContainer extends React.Component {
+
+  renderSegmentSelects() {
+    return this.props.segments.map((segment, i) => (
+      <div className="segment-select" key={i} onClick={() => this.props.addOrRemoveSegment(i)}>
+        <div className={`check ${segment.selected ? 'selected' : ''}`}></div>
+        <div className={`segment-item ${segment.selected ? 'selected' : ''}`}>{segment.name}</div>
+      </div>
+    ));
+  }
+
+  render() {
+
+    return (
+      <div className="segment-select-wrapper">
+        {this.renderSegmentSelects()}
+        
+        <div className="done-container">
+          <button className="done" onClick={this.props.close}>Done</button>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default SegmentSelectContainer;
