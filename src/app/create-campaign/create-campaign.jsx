a07d51b9448d2cd3d1a13a7dc887b2eaa6d36359
Jason Liu <jliu@Jasons-MacBook-Pro-2.local>
2019-05-22 18:44:01 -0400

    Add footer

--- src/app/create-campaign/create-campaign.jsx
+++ src/app/create-campaign/create-campaign.jsx
@@ -103,162 +103,174 @@ class CreateCampaign extends React.Component {
 
   render() {
     return (
-      <form className="page-wrapper">
-        <div className="campaign-container">
-          <header className="text">
-            <h1>Create Campaign</h1>
-            <span>Cancel</span>
-          </header>
-
-          <section>
-            <header>
-              <img src={settingsIcon} alt="settings" />
-              <h2>Settings</h2>
+      <div>
+        <form className="page-wrapper">
+          <div className="campaign-container">
+            <header className="text">
+              <h1>Create Campaign</h1>
+              <span>Cancel</span>
             </header>
 
-            <div>
-              <label>
-                Campaign Name
-                <input placeholder={'Example: The Harbor East Group Display'} className="campaign-name" type="text" onChange={this.updateFormField('name')} value={this.state.name} />
-              </label>
-            </div>
+            <section>
+              <header>
+                <img src={settingsIcon} alt="settings" />
+                <h2>Settings</h2>
+              </header>
 
-            <div className="month-controls">
-              <label>
-                Start month
-                {this.state.showStartPicker ? <MonthPicker selectedDate={this.state.startMonth} handleSelect={this.updateStartMonth} close={this.closeMonthPicker}/> : null}
-                <input className="month-picker first" type="text" onClick={(e) => this.openMonthPicker('start')} value={this.state.startMonth} readOnly={true} />
-              </label>
-
-              <label>
-                End month
-                {this.state.showEndPicker ? <MonthPicker selectedDate={this.state.endMonth} handleSelect={this.updateEndMonth} close={this.closeMonthPicker} end={true} /> : null}
-                <input className="month-picker" type="text" onClick={(e) => this.openMonthPicker('end')} value={this.state.endMonth} readOnly={true} />
-              </label>              
-            </div>
-          
-            <div className="budget-controls">
-              Budget
-              <div className="radio-container">
+              <div>
                 <label>
-                  <input className="radio" type="radio" name="budget" value="500"/>
-                  $ 500 / month
+                  Campaign Name
+                  <input placeholder={'Example: The Harbor East Group Display'} className="campaign-name" type="text" onChange={this.updateFormField('name')} value={this.state.name} />
+                </label>
+              </div>
+
+              <div className="month-controls">
+                <label>
+                  Start month
+                  {this.state.showStartPicker ? <MonthPicker selectedDate={this.state.startMonth} handleSelect={this.updateStartMonth} close={this.closeMonthPicker}/> : null}
+                  <input className="month-picker first" type="text" onClick={(e) => this.openMonthPicker('start')} value={this.state.startMonth} readOnly={true} />
                 </label>
 
                 <label>
-                  <input className="radio" type="radio" name="budget" value="500"/>
-                  $ 800 / month
-                </label>
+                  End month
+                  {this.state.showEndPicker ? <MonthPicker selectedDate={this.state.endMonth} handleSelect={this.updateEndMonth} close={this.closeMonthPicker} end={true} /> : null}
+                  <input className="month-picker" type="text" onClick={(e) => this.openMonthPicker('end')} value={this.state.endMonth} readOnly={true} />
+                </label>              
+              </div>
+            
+              <div className="budget-controls">
+                Budget
+                <div className="radio-container">
+                  <label>
+                    <input className="radio" type="radio" name="budget" value="500"/>
+                    $ 500 / month
+                  </label>
 
+                  <label>
+                    <input className="radio" type="radio" name="budget" value="500"/>
+                    $ 800 / month
+                  </label>
+
+                  <label>
+                    <input className="radio" type="radio" name="budget" value="500"/>
+                    $ 1000 / month
+                  </label>                                
+                </div>
+              </div>
+
+              <div>
                 <label>
-                  <input className="radio" type="radio" name="budget" value="500"/>
-                  $ 1000 / month
-                </label>                                
-              </div>
-            </div>
+                  Landing Page for Ad Traffic
+                  <input placeholder={'Example: harboreastgroup.com/contact'} onChange={this.updateFormField('landingPage')}  className="campaign-name" type="text" value={this.state.landingPage} />
+                </label>
+              </div>            
 
-            <div>
-              <label>
-                Landing Page for Ad Traffic
-                <input placeholder={'Example: harboreastgroup.com/contact'} onChange={this.updateFormField('landingPage')}  className="campaign-name" type="text" value={this.state.landingPage} />
-              </label>
-            </div>            
-
-            <aside className="instructions">
-              <div className="instruction-item first">
-                <img src={infoIcon} alt="info" />
-                <div className="info-text">
-                  Your campaign will begin on the first of the start month and will run through the end 
-                  month. Your campaign must run for at least 6 months. If your campaign has no end 
-                  month, it will be ongoing. You can terminate your campaign any time. 
-                </div>
-              </div>
-
-              <div className="instruction-item">
-                <img src={infoIcon} alt="info" />
-                <div className="info-text">
-                  These budget options have been selected to optimize performance at different levels
-                  of spend. The higher the monthly budget, the faster campaigns will be optimized for 
-                  performance, increasing clicks.
-                </div>
-              </div>
-            </aside>
-          </section>
-
-          <section>
-            <header>
-              <img src={locationIcon} alt="location" />
-              <h2>Location</h2>
-            </header>
-            <div>
-              <label>
-                Zip Code
-                <div className="zip">
-                  <input style={{width: '140px'}} type="number" onChange={this.updateFormField('zipCode')} value={this.state.zipCode} />
-                  <div className="instruction-item">
-                    <img src={infoIcon} alt="info" />
-                    <div className="info-text">
-                      Morgan Stanley has determined the designated marketing areas to optimize reach and target potential clients most likely to engage with your ads based on your zip code
-                    </div>
+              <aside className="instructions">
+                <div className="instruction-item first">
+                  <img src={infoIcon} alt="info" />
+                  <div className="info-text">
+                    Your campaign will begin on the first of the start month and will run through the end 
+                    month. Your campaign must run for at least 6 months. If your campaign has no end 
+                    month, it will be ongoing. You can terminate your campaign any time. 
                   </div>
                 </div>
-              </label>
-            </div>
 
-            <div className="region-container">
-              <img src={map} alt="map"/>
-              <div className="regions">
-                Regions included in the designated marketing area:
-                <ul>
-                  <li>DMA 1</li>
-                  <li>DMA 2</li>
-                  <li>DMA 3</li>
-                  <li>DMA 4</li>
-                  <li>DMA 5</li>
-                </ul>
-              </div>
-            </div>
-          </section>
-
-          <section>
-            <header>
-              <img src={targetIcon} alt="target" />
-              <h2>Targeting</h2>
-            </header>
-
-            <div className="field include-message">
-              <div style={{position: 'relative'}}>
-                Audience Segments
-                <button onClick={(e) => this.toggleSegmentSelect(e)} className="add-remove">
-                  <span>+ add / remove</span>
-                  <div className={`triangle ${!this.state.showSegmentSelect ? 'flip' : ''}`}></div>
-                </button>
-                { this.state.showSegmentSelect ? <SegmentSelectContainer addOrRemoveSegment={this.addOrRemoveSegment} close={this.toggleSegmentSelect} segments={this.state.segments}/> : null}
-              </div>
-
-              <div className="instruction-item">
-                <img src={infoIcon} alt="info" />
-                <div className="info-text">
-                  Morgan Stanley has determined the designated marketing areas to optimize reach and target potential clients most likely to engage with your ads based on your zip code
+                <div className="instruction-item">
+                  <img src={infoIcon} alt="info" />
+                  <div className="info-text">
+                    These budget options have been selected to optimize performance at different levels
+                    of spend. The higher the monthly budget, the faster campaigns will be optimized for 
+                    performance, increasing clicks.
+                  </div>
                 </div>
-              </div>              
-            </div>
+              </aside>
+            </section>
 
-            <div className="field">
-              Included
-              <div className="segments-container">
-                {this.renderSelectedSegments()}
+            <section>
+              <header>
+                <img src={locationIcon} alt="location" />
+                <h2>Location</h2>
+              </header>
+              <div>
+                <label>
+                  Zip Code
+                  <div className="zip">
+                    <input style={{width: '140px'}} type="number" onChange={this.updateFormField('zipCode')} value={this.state.zipCode} />
+                    <div className="instruction-item">
+                      <img src={infoIcon} alt="info" />
+                      <div className="info-text">
+                        Morgan Stanley has determined the designated marketing areas to optimize reach and target potential clients most likely to engage with your ads based on your zip code
+                      </div>
+                    </div>
+                  </div>
+                </label>
               </div>
-            </div>
-          </section>
 
-          <section>
-            <header>
-              <h2>Creative</h2>
-            </header>
-          </section>
-        </div>
-      </form>
+              <div className="region-container">
+                <img src={map} alt="map"/>
+                <div className="regions">
+                  Regions included in the designated marketing area:
+                  <ul>
+                    <li>DMA 1</li>
+                    <li>DMA 2</li>
+                    <li>DMA 3</li>
+                    <li>DMA 4</li>
+                    <li>DMA 5</li>
+                  </ul>
+                </div>
+              </div>
+            </section>
+
+            <section>
+              <header>
+                <img src={targetIcon} alt="target" />
+                <h2>Targeting</h2>
+              </header>
+
+              <div className="field include-message">
+                <div style={{position: 'relative'}}>
+                  Audience Segments
+                  <button onClick={(e) => this.toggleSegmentSelect(e)} className="add-remove">
+                    <span>+ add / remove</span>
+                    <div className={`triangle ${!this.state.showSegmentSelect ? 'flip' : ''}`}></div>
+                  </button>
+                  { this.state.showSegmentSelect ? <SegmentSelectContainer addOrRemoveSegment={this.addOrRemoveSegment} close={this.toggleSegmentSelect} segments={this.state.segments}/> : null}
+                </div>
+
+                <div className="instruction-item">
+                  <img src={infoIcon} alt="info" />
+                  <div className="info-text">
+                    Morgan Stanley has determined the designated marketing areas to optimize reach and target potential clients most likely to engage with your ads based on your zip code
+                  </div>
+                </div>              
+              </div>
+
+              <div className="field">
+                Included
+                <div className="segments-container">
+                  {this.renderSelectedSegments()}
+                </div>
+              </div>
+            </section>
+
+            <section>
+              <header>
+                <h2>Creative</h2>
+              </header>
+            </section>
+          </div>
+        </form>
+
+        <div className="campaign-footer">
+          <div className="left">
+            <span class="text">back to campaigns</span>
+          </div>
+          <div className="right">
+            <button className="cancel">Cancel</button>
+            <button className="save">Save Campaign</button>
+          </div>
+        </div>        
+      </div>
     );
   }
 }
