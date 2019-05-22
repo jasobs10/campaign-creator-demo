3d8582f73ced955cde409cb8d9192788e3d8f75d
Jason Liu <jliu@Jasons-MBP-2.fios-router.home>
2019-05-22 17:13:21 -0400

    Add create campaign component

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 
 import './App.css';
 import NavBar from './app/shared/navbar/navbar';
+import CreateCampaign from './app/create-campaign/create-campaign';
 
 const App = () => {
   return (
     <div>
       <NavBar />
+      <CreateCampaign />
     </div>
   );
 }
